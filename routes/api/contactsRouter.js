106cd19c2a9655439dff9772f5cf6f06420a1a9f
jmatela26 <jmatela26@gmail.com>
2024-06-05 23:27:58 +0800

    New commits for hw04 authentication

--- routes/api/contactsRouter.js
+++ routes/api/contactsRouter.js
@@ -1,19 +1,44 @@
 import express from "express";
 import { ctrlWrapper } from "../../helpers/ctrlWrapper.js";
+// prettier-ignore
 import { addContact, deleteContactById, getAllContacts, getContactById, updateContactById, updateStatusContact } from "../../controllers/contactsController.js";
+import { authenticateToken } from "../../middlewares/authenticateToken.js";
 
 const router = express.Router();
 
-router.get("/", ctrlWrapper(getAllContacts));
+/* GET: // http://localhost:3000/api/contacts */
+router.get("/", authenticateToken, ctrlWrapper(getAllContacts));
 
-router.get("/:contactId", ctrlWrapper(getContactById));
+/* GET: // http://localhost:3000/api/contacts/:contactId */
+router.get("/:contactId", authenticateToken, ctrlWrapper(getContactById));
 
-router.post("/", ctrlWrapper(addContact));
+/* POST: // http://localhost:3000/api/contacts/ 
+{
+    "name": "Josiah Matela",
+    "email": "josiahmatela@example.com",
+    "phone": "(639) 178-8856"
+} 
+*/
+router.post("/", authenticateToken, ctrlWrapper(addContact));
 
-router.delete("/:contactId", ctrlWrapper(deleteContactById));
+/* DELETE: // http://localhost:3000/api/contacts/:contactId */
+router.delete("/:contactId", authenticateToken, ctrlWrapper(deleteContactById));
 
-router.put("/:contactId", ctrlWrapper(updateContactById));
+/* PUT: // http://localhost:3000/api/contacts/:contactId 
+{
+    "name": "Josh Matela",
+    "email": "joshmatela@example.com",
+    "phone": "(639) 178-8857"
+} 
+*/
+router.put("/:contactId", authenticateToken, ctrlWrapper(updateContactById));
 
-router.patch("/:contactId/favorite", ctrlWrapper(updateStatusContact));
+/* PATCH: // http://localhost:3000/api/contacts/:contactId/favorite
+{
+    "favorite": true,
+}
+*/
+// prettier-ignore
+router.patch("/:contactId/favorite", authenticateToken, ctrlWrapper(updateStatusContact));
 
 export { router };
